cd8a2bb8645f8592975c0ca0657450b09b16e521
Seth <sddalenb@iupui.edu>
2018-11-12 15:13:58 -0500

    fixed seeding

--- packages/rumi-apiserver/app/seeds/mongodb/$default.js
+++ packages/rumi-apiserver/app/seeds/mongodb/$default.js
@@ -9,44 +9,17 @@ module.exports = Seed.extend ({
         return {name: `user-${i}`, role: `admin`};
       }),
 
-      tasks: [
-        {
-          title: 'title-0',
-          description: 'desc-0',
-          date_assigned: Date.now(),
-          date_due: Date.now(),
-          creator: dab.ref('users.1'),
-          assignee: dab.ref('users.1'),
-          status: 'not-started'
-        },
-        {
-          title: 'title-1',
-          description: 'desc-1',
-          date_assigned: Date.now(),
-          date_due: Date.now(),
-          creator: dab.ref('users.1'),
-          assignee: dab.ref('users.2'),
-          status: 'not-started'
-        },
-        {
-          title: 'title-2',
-          description: 'desc-2',
-          date_assigned: Date.now(),
-          date_due: Date.now(),
-          creator: dab.ref('users.1'),
-          assignee: dab.ref('users.3'),
-          status: 'not-started'
-        },
-        {
-          title: 'title-3',
-          description: 'desc-3',
-          date_assigned: Date.now(),
-          date_due: Date.now(),
-          creator: dab.ref('users.1'),
-          assignee: dab.ref('users.1'),
+      tasks: dab.times(10, (i) => {
+        return {
+          title: `title-${i}`,
+          description: `desc-${i}`,
+          date_assigned: Date(),
+          date_due: Date(),
+          creator: dab.ref('users.0'),
+          assignee: dab.ref(`users.${i}`),
           status: 'not-started'
         }
-      ]
+      })
     };
   }
 });
